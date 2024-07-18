e4ec518f2fc757c783bab81974984444e7f00a33
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-18 18:04:26 +0300

    a

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
-"""DB module
+"""DataBase
+
+Raises:
+    InvalidRequestError
+    NoResultFound
+    ValueError
+
+Returns
 """
+
+
+import logging
+from typing import Dict
+
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
+
+logging.disable(logging.WARNING)
 
 
 class DB:
     """DB class
     """
-
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """constructor method
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
@@ -23,22 +38,82 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """ _session property
+
+        Returns:
+            Session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add_user method
+
+        Args:
+            email (str)
+            hashed_password (str): hashed password
+
+        Returns:
+            User
         """
-        save the user to the database
-        """
-        user = User(email=email, hashed_password=hashed_password)
+
+        new_user = User(email=email, hashed_password=hashed_password)
         try:
-            self._session.add(user)
+            self._session.add(new_user)
             self._session.commit()
-        except Exception as ex:
-            print('Error adding user to database: ' + str(ex))
+        except Exception as e:
+            print(f"Error adding user to database: {e}")
             self._session.rollback()
+            raise
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find_user_by method
+
+        Raises:
+            InvalidRequestError
+            NoResultFound
+
+        Returns:
+            User
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ update_user method
+
+        Args:
+            user_id (int)
+
+        Raises:
+            ValueError
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError("User with id {} not found".format(user_id))
+
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError("User has no attribute {}".format(key))
+            setattr(user, key, value)
+
+        try:
+            self._session.commit()
+        except InvalidRequestError:
+            raise ValueError("Invalid request")
