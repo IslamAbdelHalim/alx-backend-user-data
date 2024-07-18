9c1e7895ba0265e884c579d6f01fbc5ad3e8350f
islam ahmed <eslamelbltagye1111@gmail.com>
2024-07-19 01:18:43 +0300

    update auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
+"""auth
+
+Raises:
+    ValueError
+
+Returns:
+    _type_: auth
 """
-creating a hash password
-"""
+
 
 import bcrypt
 from db import DB
-from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
+from user import User
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
-    """
-    function creating hash password
+    """_hash_password method
 
     Args:
-        password str
-    Return:
-        hashed password
-    """
+        password (str)
 
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-    return hashed_password
+    Returns:
+        str: hashed password
+    """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
 
 
 def _generate_uuid() -> str:
@@ -35,24 +41,36 @@ def _generate_uuid() -> str:
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
+    """Auth class
     """
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """register_user method
+
+        Args:
+            email (str)
+            password (str)
+
+        Raises:
+            ValueError
+
+        Returns:
+            User
         """
-            register user
-        """
+
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email, hashed_password)
-            return new_user
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
         else:
-            raise ValueError(f"User {email} already exists")
+            raise ValueError(f'User {email} already exists')
 
     def valid_login(self, email: str, password: str) -> bool:
         """valid_login method
