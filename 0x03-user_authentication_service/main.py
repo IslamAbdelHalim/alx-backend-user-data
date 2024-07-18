51328c354047c479e63a4fc8ef48ea8398e54431
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-18 18:00:04 +0300

    a

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+from db import DB
 from user import User
 
+my_db = DB()
 
-print(User.__tablename__)
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
 
-
-for column in User.__table__.columns:
-    print(column, column.type)
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
